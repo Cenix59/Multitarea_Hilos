a002608db302bfde9911e9dc2d8c802fd97f3843
Cenix59 <118486024+Cenix59@users.noreply.github.com>
2023-07-03 12:41:46 -0300

    First Commit

---
+++ Multitarea_Hilos/Multitarea_Hilos/Form1.Designer.cs
@@ -0,0 +1,129 @@
+﻿namespace Multitarea_Hilos
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.TextBox2 = new System.Windows.Forms.TextBox();
+            this.Label1 = new System.Windows.Forms.Label();
+            this.Progress = new System.Windows.Forms.ProgressBar();
+            this.Numbertxt = new System.Windows.Forms.TextBox();
+            this.CancelBtn = new System.Windows.Forms.Button();
+            this.StartBtn = new System.Windows.Forms.Button();
+            this.ProgressText = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // TextBox2
+            // 
+            this.TextBox2.Location = new System.Drawing.Point(14, 142);
+            this.TextBox2.Multiline = true;
+            this.TextBox2.Name = "TextBox2";
+            this.TextBox2.Size = new System.Drawing.Size(334, 162);
+            this.TextBox2.TabIndex = 13;
+            // 
+            // Label1
+            // 
+            this.Label1.AutoSize = true;
+            this.Label1.Location = new System.Drawing.Point(363, 43);
+            this.Label1.Name = "Label1";
+            this.Label1.Size = new System.Drawing.Size(0, 13);
+            this.Label1.TabIndex = 12;
+            // 
+            // Progress
+            // 
+            this.Progress.Location = new System.Drawing.Point(12, 37);
+            this.Progress.Name = "Progress";
+            this.Progress.Size = new System.Drawing.Size(336, 19);
+            this.Progress.TabIndex = 11;
+            // 
+            // Numbertxt
+            // 
+            this.Numbertxt.Location = new System.Drawing.Point(139, 94);
+            this.Numbertxt.Name = "Numbertxt";
+            this.Numbertxt.Size = new System.Drawing.Size(86, 20);
+            this.Numbertxt.TabIndex = 10;
+            this.Numbertxt.TextChanged += new System.EventHandler(this.Numbertxt_TextChanged);
+            // 
+            // CancelBtn
+            // 
+            this.CancelBtn.Location = new System.Drawing.Point(270, 79);
+            this.CancelBtn.Name = "CancelBtn";
+            this.CancelBtn.Size = new System.Drawing.Size(79, 35);
+            this.CancelBtn.TabIndex = 9;
+            this.CancelBtn.Text = "Cancel";
+            this.CancelBtn.UseVisualStyleBackColor = true;
+            // 
+            // StartBtn
+            // 
+            this.StartBtn.Enabled = false;
+            this.StartBtn.Location = new System.Drawing.Point(11, 79);
+            this.StartBtn.Name = "StartBtn";
+            this.StartBtn.Size = new System.Drawing.Size(79, 35);
+            this.StartBtn.TabIndex = 8;
+            this.StartBtn.Text = "Start";
+            this.StartBtn.UseVisualStyleBackColor = true;
+            this.StartBtn.Click += new System.EventHandler(this.StartBtn_Click);
+            // 
+            // ProgressText
+            // 
+            this.ProgressText.AutoSize = true;
+            this.ProgressText.Location = new System.Drawing.Point(363, 43);
+            this.ProgressText.Name = "ProgressText";
+            this.ProgressText.Size = new System.Drawing.Size(21, 13);
+            this.ProgressText.TabIndex = 14;
+            this.ProgressText.Text = "0%";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(409, 339);
+            this.Controls.Add(this.ProgressText);
+            this.Controls.Add(this.TextBox2);
+            this.Controls.Add(this.Label1);
+            this.Controls.Add(this.Progress);
+            this.Controls.Add(this.Numbertxt);
+            this.Controls.Add(this.CancelBtn);
+            this.Controls.Add(this.StartBtn);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        internal System.Windows.Forms.TextBox TextBox2;
+        internal System.Windows.Forms.Label Label1;
+        internal System.Windows.Forms.ProgressBar Progress;
+        internal System.Windows.Forms.TextBox Numbertxt;
+        internal System.Windows.Forms.Button CancelBtn;
+        internal System.Windows.Forms.Button StartBtn;
+        private System.Windows.Forms.Label ProgressText;
+    }
+}
+
---
+++ Multitarea_Hilos/Multitarea_Hilos/Form1.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Xml.Schema;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+
+namespace Multitarea_Hilos
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        string ruta = @"C:\Users\PC-1\Desktop\Multitarea_Hilos\Test\";
+        string archivo = "Prueba.txt";
+
+        private void StartBtn_Click(object sender, EventArgs e)
+        {
+            FileStream fs;
+
+            if (Numbertxt.Text == "")
+            {
+                MessageBox.Show("Complete con un número mayor a 0 para poder continuar");
+            }
+            else
+            {
+                try
+                {
+                    if (File.Exists(ruta))
+                    {
+                        fs = File.Create(ruta + archivo);
+                        fs.Close();
+                        MessageBox.Show("archivo creado correctamente", "Información", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                        Escribir();
+                    }
+                    else
+                    {
+                        Directory.CreateDirectory(ruta);
+                        fs = File.Create(ruta + archivo);
+                        fs.Close();
+                        MessageBox.Show("archivo creado correctamente", "Información", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                        Escribir();
+                    }
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message, "Error crítico", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+            }
+        }
+
+        private void Escribir()
+        {
+            int linea = Convert.ToInt32(Numbertxt.Text);
+            Progress.Maximum = linea;
+
+            Thread escribirThread = new Thread(() =>
+            {
+                using (StreamWriter escribir = new StreamWriter(ruta + archivo))
+                {
+                    try
+                    {
+                        for (int x = 1; x <= linea; x++)
+                        {
+                            //Thread.Sleep(1000);
+                            escribir.WriteLine("Linea: " + x.ToString());
+                            Progress.Invoke((MethodInvoker)(() => Progress.Value = x));
+                            int resultado = x * 100 / linea;
+                            ProgressText.Invoke((MethodInvoker)(() => ProgressText.Text = resultado.ToString() + "%"));
+                        }
+
+                        escribir.Close();
+                    }
+                    catch (Exception ex)
+                    {
+                        MessageBox.Show(ex.Message, "Error crítico", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    }
+                }
+            });
+
+            escribirThread.Start();
+        }
+
+
+        private void Numbertxt_TextChanged(object sender, EventArgs e)
+        {
+            if (string.IsNullOrEmpty(Numbertxt.Text))
+                StartBtn.Enabled = false;
+            else
+                StartBtn.Enabled = true;
+        }
+    }
+}
